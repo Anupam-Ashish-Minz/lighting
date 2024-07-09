09f833d917f2e6b6cee7bfd6654d2efd5e14ed36
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-07-09 10:40:03 +0530

    lighting is working

--- fragment.glsl
+++ fragment.glsl
@@ -1,12 +1,23 @@
 #version 330 core
 
-out vec4 color;
+in vec3 normal;
+in vec3 fragPos;
+
+out vec4 fragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
 
 void main() {
 	float ambientStrength = 0.1;
 	vec3 ambientColor = ambientStrength * lightColor;
-	color = vec4(ambientColor * objectColor, 1.0);
+
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(lightPos - fragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+
+	vec3 res = (ambientColor + diffuse) * objectColor;
+	fragColor = vec4(res, 1.0);
 }
