f69156f231e0e5ffbb128d41cfed48cd7cb4070c
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-07-10 12:30:28 +0530

    rearranging arguments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@
 #define WIDTH 1920
 #define HEIGHT 1080
 
+Camera *camera;
+
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
 								GLenum severity, GLsizei length,
 								const GLchar *message, const void *userParam) {
@@ -19,8 +21,6 @@ void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
 			(type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type,
 			severity, message);
 }
-
-Camera *camera;
 
 void MouseInputCallback(GLFWwindow *window, double xpos, double ypos) {
 	camera->process_mouse_input(window, xpos, ypos);
