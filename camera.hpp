56fdf5c49ff1ae80f46d2fcb724499faa54ccaaf
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-07-13 23:23:39 +0530

    change camera starting position

--- camera.hpp
+++ camera.hpp
@@ -12,7 +12,7 @@ class Camera {
   private:
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 8.0f);
+	glm::vec3 cameraPos = glm::vec3(0.0f, -2.0f, 8.0f);
 
 	bool firstMouse = true;
 	float lastX, lastY;
